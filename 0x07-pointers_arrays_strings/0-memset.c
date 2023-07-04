6ffc974213fe7640a4f9c078a434fc7e422e31e8
saidmera <55964968+saidmera@users.noreply.github.com>
2023-07-04 08:22:20 +0100

    Update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * _memset - fill a block of memory
- * @s: starting address of memory
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
  * @b: the desired value
- * @n: member of bytes to be changed
+ * @n: number of bytes to be changed
  *
- * Return: Always value
+ * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	while (i < n)
+	for (; n > 0; i++)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b;
+		n--;
 	}
 	return (s);
 }
+
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * _memcpy - a function that copies memory area
- * @dest: memory where is stored
- * @src: memory where is copies
- * @n: member of bytes
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
  *
- * Return: copied memory with n byted changed
+ *Return: copied memory with n bytes changed
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
@@ -19,3 +18,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		n--;
 	}
 	return (dest);
+}
+
